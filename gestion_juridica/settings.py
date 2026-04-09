1dddf0160aa0e185e8519487eb08dca2280a917c
Emanuel Jared Venegas Perez <emanuelvperez2000@gmail.com>
2026-04-09 17:13:10 -0400

    xd

--- gestion_juridica/settings.py
+++ gestion_juridica/settings.py
@@ -76,27 +76,27 @@ WSGI_APPLICATION = 'gestion_juridica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'gestion_j',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE':'django.db.backends.postgresql_psycopg2',
-#         'NAME':'gestion_aj',
-#         'USER':'gestion_aj',
-#         'PASSWORD':'gestion_2025',
+#         'NAME':'gestion_j',
+#         'USER':'postgres',
+#         'PASSWORD':'1234',
 #         'HOST':'localhost',
 #         'PORT':'5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'gestion_aj',
+        'USER':'gestion_aj',
+        'PASSWORD':'gestion_2025',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
 
 
 # Password validation
